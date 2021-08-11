1e76c5e0e4d65a14167a31b5bdcd6a6a1eebdc65
Higor Christian <higorchristianfe@gmail.com>
2021-08-11 11:25:01 -0300

    plugin architecture

---
+++ easy-lgpd.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name:       Easy Lgpd
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Suitability to lgpd.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Higor Christian
+ * Author URI:        https://www.linkedin.com/in/higorch/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       easylgpd
+ * Domain Path:       /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+define('EASY_LGPD_URL', plugin_dir_url(__FILE__));
+define('EASY_LGPD_PATH', plugin_dir_path(__FILE__));
+
+require_once EASY_LGPD_PATH . 'includes/helpers.php';
