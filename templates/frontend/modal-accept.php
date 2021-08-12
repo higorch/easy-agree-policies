1dac6c62c6bb47a16767815bc8263f8bf2134b8b
Higor Christian <higorchristianfe@gmail.com>
2021-08-12 18:22:48 -0300

    frontend

---
+++ templates/frontend/modal-accept.php
@@ -0,0 +1,17 @@
+<div class="modal-accept">
+
+    <div class="body">
+
+        <div class="box">
+            <div class="col info">
+                <strong>Politica & Privacidade</strong>
+                <p>Este site usa cookies para melhorar sua experiência. Ao continuar navegando você concorda com o uso dos cookies, termos e políticas do site. <a href="#">Leia Mais</a></p>
+            </div>
+            <div class="col btn">
+                <button type="button">Aceitar</button>
+            </div>
+        </div>
+
+    </div>
+
+</div>
