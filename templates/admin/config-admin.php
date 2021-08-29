3ffcd1d160571a511cfea95a3dfd4a9e4f137d9b
higorch <higorchristianfe@gmail.com>
2021-08-29 12:10:03 -0300

    Admin tag manager

--- templates/admin/config-admin.php
+++ templates/admin/config-admin.php
@@ -2,6 +2,7 @@
 $geral_screen = (!isset($_GET['action']) || isset($_GET['action']) && 'geral' == $_GET['action']) ? true : false;
 $styles_screen = (isset($_GET['action']) && 'styles' == $_GET['action']) ? true : false;
 $link_generator_screen = (isset($_GET['action']) && 'link-generator' == $_GET['action']) ? true : false;
+$tag_manager = (isset($_GET['action']) && 'tag-manager' == $_GET['action']) ? true : false;
 ?>
 <div class="wrap">
 
@@ -11,6 +12,7 @@ $link_generator_screen = (isset($_GET['action']) && 'link-generator' == $_GET['a
         <a href="<?php echo esc_url(add_query_arg(array('action' => 'geral'), admin_url('admin.php?page=easy-agree-policies'))); ?>" class="nav-tab<?php if ($geral_screen) echo ' nav-tab-active'; ?>"><?php esc_html_e('Geral', 'easyap'); ?></a>
         <a href="<?php echo esc_url(add_query_arg(array('action' => 'styles'), admin_url('admin.php?page=easy-agree-policies'))); ?>" class="nav-tab<?php if ($styles_screen) echo ' nav-tab-active'; ?>"><?php esc_html_e('Estilos', 'easyap'); ?></a>
         <a href="<?php echo esc_url(add_query_arg(array('action' => 'link-generator'), admin_url('admin.php?page=easy-agree-policies'))); ?>" class="nav-tab<?php if ($link_generator_screen) echo ' nav-tab-active'; ?>"><?php esc_html_e('Gerador de link', 'easyap'); ?></a>
+        <a href="<?php echo esc_url(add_query_arg(array('action' => 'tag-manager'), admin_url('admin.php?page=easy-agree-policies'))); ?>" class="nav-tab<?php if ($tag_manager) echo ' nav-tab-active'; ?>"><?php esc_html_e('Tags', 'easyap'); ?></a>
     </h2>
 
     <form method="post" action="options.php">
@@ -29,7 +31,11 @@ $link_generator_screen = (isset($_GET['action']) && 'link-generator' == $_GET['a
         }
 
         if ($link_generator_screen) {
-            require_once EASYAP_PATH . 'templates/admin/partials/link-generator.php';            
+            require_once EASYAP_PATH . 'templates/admin/partials/link-generator.php';
+        }
+
+        if ($tag_manager) {
+            require_once EASYAP_PATH . 'templates/admin/partials/tag-manager.php';
         }
         ?>
 
