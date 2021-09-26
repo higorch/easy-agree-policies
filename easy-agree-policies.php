d69684ee8f4750dbc2eff9eec5a4422952aadd83
higorch <higorchristianfe@gmail.com>
2021-09-26 19:55:32 -0300

    migrations

--- easy-agree-policies.php
+++ easy-agree-policies.php
@@ -19,9 +19,12 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-define('EASYAP_URL', plugin_dir_url(__FILE__));
-define('EASYAP_PATH', plugin_dir_path(__FILE__));
+define('EASYAP_DB_VERSION', '1.0');
+define('EASYAP_FILE', __FILE__);
+define('EASYAP_URL', plugin_dir_url(EASYAP_FILE));
+define('EASYAP_PATH', plugin_dir_path(EASYAP_FILE));
 
 require_once EASYAP_PATH . 'includes/helpers.php';
+require_once EASYAP_PATH . 'includes/class-migration.php';
 require_once EASYAP_PATH . 'includes/class-admin.php';
 require_once EASYAP_PATH . 'includes/class-frontend.php';
