59b314057fe3e6869e15250e70a934a31a967940
Higor Christian <higorchristianfe@gmail.com>
2022-02-05 11:48:29 -0300

    frontend modal cookies

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,7 +1,21 @@
 (function ($) {
 
+    // open modal 
     $('.modal-accept .body .box .col.btn button.options').on('click', function () {
         $('.modal-cookies').addClass('active');
+        $("body").css("overflow", "hidden");
+    });
+
+    // close modal
+    $(document).on('click', '.modal-cookies', function (e) {
+
+        var el = $(this);
+
+        if (!$(e.target).closest('.modal-cookies > *').length || $(e.target).hasClass('dialog') || $(e.target).closest('.modal-cookies .dialog .content .close').length) {
+            el.removeClass('active');
+            $("body").css("overflow", "initial");
+        }
+
     });
 
 })(jQuery);
