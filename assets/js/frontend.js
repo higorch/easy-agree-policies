cb80e396cfe4d08c782c9e5902297da011a74933
Higor Christian <higorchristianfe@gmail.com>
2022-02-05 10:42:03 -0300

    modal accept

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,4 +1,7 @@
 (function ($) {
-    
-    
+
+    $('.modal-accept .body .box .col.btn button.options').on('click', function () {
+        $('.modal-cookies').addClass('active');
+    });
+
 })(jQuery);
